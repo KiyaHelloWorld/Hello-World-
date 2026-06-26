2f29bc9abfb521719c300fadf8fe802219285537
valenciakiara033 <Valenciakiara033@gmail.com>
2026-06-26 13:21:13 +0800

    JAVAVAVAVAV

---
+++ Websites/Javascript - Review/index.js
@@ -0,0 +1,9 @@
+let firstName = "Analene"
+let lastName = "Panlipunan"
+let age = "19"
+let program = "Bachelor's of Science in Computer Science Major in Network and Information Technology"
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(program)
