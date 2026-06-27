7d92a3ad4a9ffc7acbbbefebd73d6a1ef7a153f1
valenciakiara033 <Valenciakiara033@gmail.com>
2026-06-27 23:53:11 +0800

    this day!

--- Websites/Javascript - Review/index.js
+++ Websites/Javascript - Review/index.js
@@ -1,3 +1,6 @@
+
+// Variables 
+//Variable Initialization
 let firstName = "Analene"
 let lastName = "Panlipunan"
 let age = "19"
@@ -7,3 +10,18 @@ console.log(firstName)
 console.log(lastName)
 console.log(age)
 console.log(program)
+
+// Const and Let
+// Const (Constant) - Values that should not be changed should be stored in a constant variable
+const interest = 3.539;
+const pi = 3.1416;
+
+//Section 1 [Data Types]
+
+//String = Series of characters that create a word, phrase and/or sentence
+let mystring = "";
+let country = "United States of America"
+let state = "Los Angeles, California"
+
+console
+
