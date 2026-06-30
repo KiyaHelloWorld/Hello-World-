a524985f97577660054f619c1d988abd8bacdb71
valenciakiara033 <Valenciakiara033@gmail.com>
2026-06-30 20:41:56 +0800

    fix this plsgit add .

---
+++ Files/Javascript - Review 2/index.js
@@ -0,0 +1,20 @@
+//console.log("Hello World!")
+
+function returnFullName(){
+	let fullName = "Analene" + " " + "Panlipunan";
+	return fullName;
+}
+
+let fullName = returnFullName();
+console.log(fullName)
+
+function greet(name){
+	return "Hello " + name;
+}
+
+let messageOne = greet("Bogart");
+console.log(messageOne)
+
+//RETURN = Help you use what the function produces
+
+
---
+++ Files/Javascript - Review 3/index.js
@@ -0,0 +1,16 @@
+console.log("Earl")log
+
+let num = 90;
+
+if (num >= 85 || num >= 90){
+	console.log("With Honors!");
+}
+else if(num >= 91 || >= 95){
+	console.log("With High Honors");
+}
+else if(num >= 96 || >= 100){
+	console.log("With Highest Honors");
+}
+else{
+	console.log("No Award");
+}
